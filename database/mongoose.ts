152521b9788caea1e4d0f7b74e5fa6f56b091c53
Aimilio <aimilio.tsompa@gmail.com>
2025-11-23 15:25:18 +0200

    Fixing extra changes

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -16,7 +16,7 @@ if(!cached){
 }
 
 export const connectToDatabase=async()=>{
-    if(!MONGODB_URI) throw new Error('MongoDB_URI must be set within .env');
+    if(!MONGODB_URI) throw new Error('MONGODB_URI must be set within .env');
     if(cached.conn) return cached.conn;
 
     if(!cached.promise){
@@ -31,4 +31,5 @@ export const connectToDatabase=async()=>{
     }
 
     console.log(`Connected to Database${process.env.NODE_ENV}-${MONGODB_URI}`);
+    return cached.conn;
 }
